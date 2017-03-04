a3e2f5367d4dd3729203741ab0f68703a5cf01d2
Sean Lyons <sean@commonbond.co>
2017-03-04 14:48:43 -0500

    Fixed enums

--- src/main/scala/GrammarTypes.scala
+++ src/main/scala/GrammarTypes.scala
@@ -1,89 +1,185 @@
-sealed trait Phoneme
-case class AA extends Phoneme
-case class AE extends Phoneme
-case class AH extends Phoneme
-case class AO extends Phoneme
-case class AW extends Phoneme
-case class AY extends Phoneme
-case class B extends Phoneme
-case class CH extends Phoneme
-case class D extends Phoneme
-case class DH extends Phoneme
-case class EH extends Phoneme
-case class ER extends Phoneme
-case class EY extends Phoneme
-case class F extends Phoneme
-case class G extends Phoneme
-case class HH extends Phoneme
-case class IH extends Phoneme
-case class IY extends Phoneme
-case class JH extends Phoneme
-case class K extends Phoneme
-case class L extends Phoneme
-case class M extends Phoneme
-case class N extends Phoneme
-case class NG extends Phoneme
-case class OW extends Phoneme
-case class OY extends Phoneme
-case class P extends Phoneme
-case class R extends Phoneme
-case class S extends Phoneme
-case class SH extends Phoneme
-case class T extends Phoneme
-case class TH extends Phoneme
-case class UH extends Phoneme
-case class UW extends Phoneme
-case class V extends Phoneme
-case class W extends Phoneme
-case class Y extends Phoneme
-case class Z extends Phoneme
-case class ZH extends Phoneme
+import Phoneme.Phoneme
+import Stress.Stress
 
-sealed trait Stress
-case class None extends Stress
-case class Primary extends Stress
-case class Secondary extends Stress
+//Phonemes
+object Phoneme {
+
+  sealed trait Phoneme
+
+  object AA extends Phoneme
+
+  object AE extends Phoneme
+
+  object AH extends Phoneme
+
+  object AO extends Phoneme
+
+  object AW extends Phoneme
+
+  object AY extends Phoneme
+
+  object B extends Phoneme
+
+  object CH extends Phoneme
+
+  object D extends Phoneme
+
+  object DH extends Phoneme
+
+  object EH extends Phoneme
+
+  object ER extends Phoneme
+
+  object EY extends Phoneme
+
+  object F extends Phoneme
+
+  object G extends Phoneme
+
+  object HH extends Phoneme
+
+  object IH extends Phoneme
+
+  object IY extends Phoneme
+
+  object JH extends Phoneme
+
+  object K extends Phoneme
+
+  object L extends Phoneme
+
+  object M extends Phoneme
+
+  object N extends Phoneme
+
+  object NG extends Phoneme
+
+  object OW extends Phoneme
+
+  object OY extends Phoneme
+
+  object P extends Phoneme
+
+  object R extends Phoneme
+
+  object S extends Phoneme
+
+  object SH extends Phoneme
+
+  object T extends Phoneme
+
+  object TH extends Phoneme
+
+  object UH extends Phoneme
+
+  object UW extends Phoneme
+
+  object V extends Phoneme
+
+  object W extends Phoneme
+
+  object Y extends Phoneme
+
+  object Z extends Phoneme
+
+  object ZH extends Phoneme
+
+}
+
+//Phoneme stress type
+object Stress {
+
+  sealed trait Stress
+
+  object None extends Stress
+
+  object Primary extends Stress
+
+  object Secondary extends Stress
+
+}
 
 case class StressedPhoneme(p: Phoneme, stress: Option[Stress])
 case class Pronunciation(word: String, sps: Seq[StressedPhoneme])
 
-sealed trait POS
-case class CC extends POS
-case class CD extends POS
-case class DT extends POS
-case class EX extends POS
-case class FW extends POS
-case class IN extends POS
-case class JJ extends POS
-case class JJR extends POS
-case class JJS extends POS
-case class LS extends POS
-case class MD extends POS
-case class NN extends POS
-case class NNS extends POS
-case class NNP extends POS
-case class NNPS extends POS
-case class PDT extends POS
-case class POS extends POS
-case class PRP extends POS
-case class PRP$ extends POS
-case class RB extends POS
-case class RBR extends POS
-case class RBS extends POS
-case class RP extends POS
-case class SYM extends POS
-case class TO extends POS
-case class UH extends POS
-case class VB extends POS
-case class VBZ extends POS
-case class VBP extends POS
-case class VBD extends POS
-case class VBN extends POS
-case class VBG extends POS
-case class WDT extends POS
-case class WP extends POS
-case class WP$ extends POS
-case class WRB extends POS
+//Parts of speech
+object POS {
+
+  sealed trait POS
+
+  object CC extends POS
+
+  object CD extends POS
+
+  object DT extends POS
+
+  object EX extends POS
+
+  object FW extends POS
+
+  object IN extends POS
+
+  object JJ extends POS
+
+  object JJR extends POS
+
+  object JJS extends POS
+
+  object LS extends POS
+
+  object MD extends POS
+
+  object NN extends POS
+
+  object NNS extends POS
+
+  object NNP extends POS
+
+  object NNPS extends POS
+
+  object PDT extends POS
+
+  object POS extends POS
+
+  object PRP extends POS
+
+  object PRP$ extends POS
+
+  object RB extends POS
+
+  object RBR extends POS
+
+  object RBS extends POS
+
+  object RP extends POS
+
+  object SYM extends POS
+
+  object TO extends POS
+
+  object UH extends POS
+
+  object VB extends POS
+
+  object VBZ extends POS
+
+  object VBP extends POS
+
+  object VBD extends POS
+
+  object VBN extends POS
+
+  object VBG extends POS
+
+  object WDT extends POS
+
+  object WP extends POS
+
+  object WP$ extends POS
+
+  object WRB extends POS
+
+}
 /*
 .	punctuation mark, sentence closer	.;?* 
 ,	punctuation mark, comma	, 
